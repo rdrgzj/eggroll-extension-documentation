74d9db112312d203c0419b0036e46d2329ef2b0e
Jana Rodriguez <janafaye.rodriguez@gmail.com>
2025-01-06 13:16:44 +0800

    update

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Eggroll Game",
+    version="0.1.0",
+    author="Leandro Asunan and Jana Rodriguez",
+    description="Guide the eggs 🥚 to the nests 🪹 before you run out of moves. Avoid obstacles like frying pans 🍳.",
+    url="https://github.com/rdrgzj/eggroll-extension-documentation",
+    classifiers=[
+        "Programming Language :: Python :: 3.13",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "sphinx==8.1.3",
+        "sphinx-rtd-theme==3.0.2",
+    ],
+    extras_require={
+        'docs': [
+            'sphinx==8.1.3',
+            'sphinx-rtd-theme==3.0.2',
+            'sphinx-autodoc-typehints',
+        ],
+        'dev': [
+            'termcolor==2.5.0',
+        ],
+    }
+)
